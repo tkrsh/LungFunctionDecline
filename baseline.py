edaf135f6bc8f6398f76258c79c51654fbbc7bc3
Utkarsh <utkarsh.srivastava123@outlook.com>
2020-07-29 03:43:00 +0530

    DD

--- baseline.py
+++ baseline.py
@@ -5,63 +5,72 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
 import os
-from sklearn.cluster import KMeans
 import tensorflow as tf
 import tensorflow.keras.backend as K
 import tensorflow.keras.layers as L
 from   tensorflow.keras import Sequential
-main_dir=("/home/tkrsh/osic-main/")
+from sklearn.preprocessing import LabelEncoder
 
-files=[]
+def generate_train_df(df):
+    train_data=df.drop("Patient",axis=1)
+    train_data=train_data.drop("FVC",axis=1)
+    train_target=df.pop("FVC")
+    train_data["Sex"]=le.fit_transform(df["Sex"])
+    train_data["SmokingStatus"]=le.fit_transform(df["SmokingStatus"])
+    dataset = tf.data.Dataset.from_tensor_slices((train_data.values, train_target.values))
+    train_dataset=dataset.batch(356)
+    return train_dataset 
 
-for dirname, _, filenames in os.walk(main_dir):
-    for filename in (filenames):
-        files.append(os.path.join((dirname), filename))
+C1, C2 = tf.constant(70, dtype='float32'), tf.constant(1000, dtype="float32")
 
-files=[x for x in files if '.csv' not in x]
+def kloss(y_true, y_pred):
+    tf.dtypes.cast(y_true, tf.float32)
+    tf.dtypes.cast(y_pred, tf.float32)
+    sigma = y_pred[:, 1]
+    fvc_pred = y_pred[:, 0]
+    
+    sigma_clip = sigma + C1
+    #sigma_clip = tf.maximum(sigma, C1)
+    delta = tf.abs(y_true[:, 0] - fvc_pred)
+    #delta = tf.minimum(delta, C2)
+    sq2 = tf.sqrt( tf.dtypes.cast(2, dtype=tf.float32) )
+    metric = (delta / sigma_clip)*sq2 + tf.math.log(sigma_clip* sq2)
+    return K.mean(metric)
 
-train_images= [str(x) for x in files if 'train'  in x]
-test_images = [str(x) for x in files if 'test'   in x]
+def kmae(y_true, y_pred):
+    tf.dtypes.cast(y_true, tf.float32)
+    tf.dtypes.cast(y_pred, tf.float32)
+    spread = tf.abs( (y_true[:, 0] -  y_pred[:, 0])  / (y_pred[:, 0] + 1.) )
+    #spread = tf.abs( (y_true[:, 0] -  y_pred[:, 0])  / y_true[:, 0] )
+    return K.mean(spread)
 
-train=pd.read_csv(main_dir+"train.csv")
+def mloss(_lambda):
+    def loss(y_true, y_pred):
+        return _lambda * kloss(y_true, y_pred) + (1 - _lambda)*kmae(y_true, y_pred)
+    return loss
 
-df=train.copy()
-
-df=pd.concat([df,pd.get_dummies(df['Sex'])],axis=1).drop(['Sex'],axis=1)
-df=pd.concat([df,pd.get_dummies(df['SmokingStatus'])],axis=1).drop(['SmokingStatus'],axis=1)
-df['dFVC'] =df["FVC"]-df["FVC"].shift(1)
-df['d%'] = df["Percent"]-df["Percent"].shift(1)
-df['Gap'] = df["Weeks"]-df["Weeks"].shift(1)
-df.fillna(0,inplace=True)
-Means=KMeans(n_clusters=3).fit((df["Age"].values).reshape(-1,1))
-df["Age_Cat"]=Means.labels_
-df=pd.concat([df,pd.get_dummies(df['Age_Cat'],prefix="Age_Cat")],axis=1).drop(['Age_Cat'],axis=1)
-train_df=df.drop("Patient",axis=1)
-df["Gap"]= [int(x) for x in df["Gap"]]
-x_train=df.drop("Patient",axis=1)
-x_train=x_train.drop("Age",axis=1)
-x_train=x_train.drop("FVC",axis=1)
 def get_compiled_model():
-    model = tf.keras.Sequential([
-    tf.keras.layers.Dense(100, activation='relu'),
-    tf.keras.layers.Dense(100, activation='relu'),
-    tf.keras.layers.Dense(100, activation='relu'),
-    tf.keras.layers.Dense(100, activation='relu'),
-    tf.keras.layers.Dense(100, activation='relu'),
-        
-    tf.keras.layers.Dense(1)
-    ])
-
-    model.compile(optimizer='adam',
-            loss="mse",
-            metrics=["Mean"])
+    model = Sequential([
+    L.Dense(1024, activation='relu'),
+    L.Dense(1024, activation='relu'),
+    L.Dense(1024, activation='relu'),
+    L.Dense(256, activation='relu'),
+    L.Dense(256, activation='relu'),
+    L.Dense(256, activation='relu'),
+    L.Dense(126, activation='relu'),
+    L.Dense(16, activation='relu'),
+    tf.keras.layers.Dense(2)])
+    model.compile(loss=mloss(0.5), optimizer="adam", metrics=[kloss])
     return model
-tf.keras.backend.clear_session()
 
-y_train=df.pop("FVC")
-dataset = tf.data.Dataset.from_tensor_slices((x_train.values, y_train.values))
-train_dataset=dataset.batch(9)
-model = get_compiled_model()
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="/home/tkrsh/osic-git/", profile_batch=5)
+def run_model():
+    model = get_compiled_model()
+    # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="/home/tkrsh/osic-git/", profile_batch=5)
+    history=model.fit(generate_train_df(df),epochs=2000)
 
-model.fit(train_dataset,epochs=20000,callbacks=[tensorboard_callback])
+if __name__== "__main__":
+
+    K.clear_session()
+    le=LabelEncoder()
+    df=pd.read_csv("/home/tkrsh/osic-git/csvs/train.csv")
+    run_model()
--- notify.py
+++
@@ -1,9 +0,0 @@
-from knockknock import discord_sender
-
-webhook_url = "https://discordapp.com/api/webhooks/737146056017575968/UzcX3__SbysSIjQ_O2BGK7ti1h-45yLIV893G6ha23cWntoV9bt3Tv7E1lMcnEu2GZ0m"
-@discord_sender(webhook_url=webhook_url)
-def train_your_nicest_model():
-    import time
-    time.sleep(10000)
-    return {'loss': 0.9} # Optional return value
-train_your_nicest_model
